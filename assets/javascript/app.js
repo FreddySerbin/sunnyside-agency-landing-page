0e45cf57d75cdb4d572e2ac908956b8512394155
FreddySerbin <freddyserbin@gmail.com>
2021-10-23 19:57:41 +0200

    responsive & javascript

---
+++ assets/javascript/app.js
@@ -0,0 +1,21 @@
+
+var buttonBurger = document.querySelector('.burger');
+var menuburger = document.querySelector('.burger__menu');
+buttonBurger.addEventListener('click', openNav);
+
+function openNav() {
+    menuburger.style.display = "flex";
+}
+
+function closeNav() {
+    menuburger.style.display = "block";
+}
+
+
+
+
+
+
+
+
+
